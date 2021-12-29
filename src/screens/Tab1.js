c5c880a7848accb6f4b960e98ea2b3e440bfc140
bhasi <anilbhasib@gmail.com>
2021-12-29 13:10:14 +0530

    react base will be removed

--- src/screens/Tab1.js
+++ src/screens/Tab1.js
@@ -1,5 +1,5 @@
-import React,{useState,useEffect} from 'react'
-import { StyleSheet,Alert } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Alert ,View} from 'react-native'
 import {
     Box,
     FlatList,
@@ -9,64 +9,25 @@ import {
     Text,
     Spacer,
     NativeBaseProvider,
-    Button
+    Button,
+    Container
 } from "native-base"
 import {
     getArticles
 } from '../service/news'
 const Example = () => {
-    const [news,setNews] = useState(null)
-    const [isLoading,setIsLoading] = useState(true)
-    useEffect(()=>{
-        getArticles().then(data=>{
+    const [news, setNews] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        getArticles().then(data => {
             setNews(data)
             setIsLoading(false)
-        },error=>{
-            Alert.alert('Error','Something went wrong!!')
+        }, error => {
+            Alert.alert('Error', 'Something went wrong!!')
         })
-    },[])
-    console.log(news);
-    const data = [
-        {
-            id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-            fullName: "Aafreen Khan",
-            timeStamp: "12:47 PM",
-            recentText: "Good Day!",
-            avatarUrl:
-                "https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-        },
-        {
-            id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-            fullName: "Sujitha Mathur",
-            timeStamp: "11:11 PM",
-            recentText: "Cheer up, there!",
-            avatarUrl:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTyEaZqT3fHeNrPGcnjLLX1v_W4mvBlgpwxnA&usqp=CAU",
-        },
-        {
-            id: "58694a0f-3da1-471f-bd96-145571e29d72",
-            fullName: "Anci Barroco",
-            timeStamp: "6:22 PM",
-            recentText: "Good Day!",
-            avatarUrl: "https://miro.medium.com/max/1400/0*0fClPmIScV5pTLoE.jpg",
-        },
-        {
-            id: "68694a0f-3da1-431f-bd56-142371e29d72",
-            fullName: "Aniket Kumar",
-            timeStamp: "8:56 PM",
-            recentText: "All the best",
-            avatarUrl:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSr01zI37DYuR8bMV5exWQBSw28C1v_71CAh8d7GP1mplcmTgQA6Q66Oo--QedAN1B4E1k&usqp=CAU",
-        },
-        {
-            id: "28694a0f-3da1-471f-bd96-142456e29d72",
-            fullName: "Kiara",
-            timeStamp: "12:47 PM",
-            recentText: "I will call today.",
-            avatarUrl:
-                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRBwgu1A5zgPSvfE83nurkuzNEoXs9DMNr8Ww&usqp=CAU",
-        },
-    ]
+    }, [])
+    // console.log(news[0].author);
+
     return (
         <Box
             w={{
@@ -75,15 +36,12 @@ const Example = () => {
             }}
         >
             <FlatList
-                data={data}
+                data={news}
                 renderItem={({ item }) => (
                     <Box
                         borderBottomWidth="1"
-                        _dark={{
-                            borderColor: "gray.600",
-                        }}
                         borderColor="coolGray.200"
-                        pl="4"
+                        pl="3"
                         pr="5"
                         py="2"
                     >
@@ -91,18 +49,16 @@ const Example = () => {
                             <Avatar
                                 size="48px"
                                 source={{
-                                    uri: item.avatarUrl,
+                                    uri: item.urlToImage,
                                 }}
                             />
                             <VStack>
                                 <Text
-                                    _dark={{
-                                        color: "warmGray.50",
-                                    }}
+
                                     color="coolGray.800"
                                     bold
                                 >
-                                    {item.fullName}
+                                    {item.title}
                                 </Text>
                                 <Text
                                     color="coolGray.600"
@@ -110,17 +66,19 @@ const Example = () => {
                                         color: "warmGray.200",
                                     }}
                                 >
-                                    {item.recentText}
+                                    {item.description}
                                 </Text>
+                           
                             </VStack>
                             <Spacer />
+                            <Text>xxx</Text>
                             <Button onPress={() => console.log("hello world")}
                                 size={'md'}
                             >View</Button>
                         </HStack>
                     </Box>
                 )}
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item.title}
             />
         </Box>
     )
@@ -128,9 +86,18 @@ const Example = () => {
 const Tab1 = () => {
     return (
         <NativeBaseProvider>
-            <Example />
+            <View style={styles.container}>
+             <Example />
+            </View>
+               
+           
+
         </NativeBaseProvider>
     )
 }
 export default Tab1
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        marginRight:25
+    }
+})
