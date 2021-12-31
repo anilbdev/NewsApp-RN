692e16b6511281040c18788ed7cdfc3bbb74342a
bhasi <anilbhasib@gmail.com>
2021-12-31 10:09:26 +0530

    code clean up

--- src/service/news.js
+++ src/service/news.js
@@ -1,19 +1,13 @@
 import {
- articles_url,country_code,_api_key
+    articles_url, country_code, _api_key
 } from '../config/rest_config'
-
-export async function getArticles(category){
+export async function getArticles(category) {
     try {
-        console.log('try block>-->');
-        // let articles = await fetch(`${articles_url}?country=${country_code}&category=${category}&apiKey=78c1a7855c92490180aa0352f14a7114`)
         let articles = await fetch(`${articles_url}?country=${country_code}&category=${category}&apiKey=${_api_key}`)
-       
         let result = await articles.json()
-        // console.log('try block articles>-->',result);
         articles = null
         return result.articles
     } catch (error) {
-        console.log('error block>-->');
-        throw(error)
+        throw (error)
     }
 }
