06b23deaf8be18f6e67f9740abb95dee5ccf3454
bhasi <anilbhasib@gmail.com>
2021-12-31 10:02:49 +0530

    general business tech tabs updated

--- src/screens/Tab2.js
+++ src/screens/Tab2.js
@@ -1,14 +1,59 @@
-import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react'
+import { StyleSheet, Alert, View,ActivityIndicator,Text } from 'react-native'
 
+import {
+    getArticles,
+} from '../service/news'
+import {
+    NewsItem
+} from '../index'
 const Tab2 = () => {
+    const [news, setNews] = useState([])
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        getArticles('business').then(data => {
+            setNews(data)
+            setIsLoading(false)
+        }, error => {
+            Alert.alert('Error', 'Something went wrong!!')
+        })
+    }, [])
+
+    let view = isLoading?(
+        <View style={styles.activityIndicator}>
+            <ActivityIndicator animating={isLoading} />
+            <Text style={{marginVertical:10}}>Please wait....</Text>
+        </View>
+    ):<NewsItem news={news} />
     return (
-        <View>
-            <Text>Tab 2</Text>
+        <View style={{ flex: 1 }}>
+            {view}
         </View>
     )
 }
-
 export default Tab2
-
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        padding: 5,
+        marginVertical: 5
+    },
+    imageContainer: {
+        alignItems: 'center',
+        marginRight: 5
+    },
+    mainContent: {
+        flex: 1,
+    },
+    buttonContainer: {
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    activityIndicator:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+    }
+})
